a3966c078dc0ecca2a326a723b45d94177aa004b
SwarnabhaBiswas <swarnabha8481@gmail.com>
2024-06-10 22:25:26 +0530

    added files and folders

---
+++ SwarnabhaBiswasPRODIGY_WD_01/script.js
@@ -0,0 +1,5 @@
+gsap.from('.link',{opacity:0, duration:1, delay:1, y:10});
+gsap.from('.left__content',{opacity:0, duration:1, delay:1.5, x:-30});
+gsap.from('.right__content',{opacity:0, duration:1, delay:1.5, x:30});
+gsap.from('.icon',{opacity:0, duration:1, delay:3, x:-25});
+
